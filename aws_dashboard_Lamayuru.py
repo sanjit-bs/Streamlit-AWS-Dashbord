85a94adfb64f460b7d0c8e0f2a04a0db2bb600c0
sanjit-bs <sanjitava.22260402016@cuj.ac.in>
2025-09-07 00:06:02 +0530

    Update aws_dashboard_Lamayuru.py

--- aws_dashboard_Lamayuru.py
+++ aws_dashboard_Lamayuru.py
@@ -95,7 +95,7 @@ if not filtered_df.empty:
             r="Count",
             theta="Wind_Direction",
             color="Count",
-            color_continuous_scale=px.colors.sequential.Plasma,
+            color_continuous_scale=px.colors.sequential.Rainbow,
             title="Wind Directions",
             width=700,   # set desired width
             height=700   # set desired height
@@ -109,6 +109,7 @@ if not filtered_df.empty:
 else:
 
     st.warning("⚠️ No data available for the selected date range.")
+
 
 
 
